f8165461d50d8b1cce1b3f8444156d951c987e0c
CodingCas <casweeno2000@gmail.com>
2022-12-30 22:44:46 +0100

    Working on action files

--- app/Actions/Group/GroupHandler.php
+++ app/Actions/Group/GroupHandler.php
@@ -4,13 +4,23 @@ namespace App\Actions\Group;
 
 class GroupHandler
 {
-    public static function createGroup()
+    public static function createGroup($request)
     {
-
+        return Group::create([
+            'reference' => Str::ulid(),
+            'group_number' => $request->group_number
+        ]);
     }
 
     public static function groups()
     {
-        
+        return Group::all();
+    }
+
+    public static function assignStudentsGroup($request)
+    {
+        $user = User::where(['id' => $request->user_id, 'role' => 'STUDENT']);
+        $user->update(['group_id' => $request->group_id]);
+        return $user;
     }
 }
---
+++ app/Actions/Task/TaskHandler.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Actions\Task;
+
+class TaskHandler
+{
+    public static function assignIndividualTask()
+    {
+
+    }
+
+    public static function assignGroupTask()
+    {
+
+    }
+}
